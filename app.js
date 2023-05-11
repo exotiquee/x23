20b5e16d7ae07d5b011a7d60c80d67b012e7deab
exotiquee <kashif02ahmed@hotmail.com>
2023-05-11 18:04:46 +0200

    still got issues MAN

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const { ipcRenderer } = require('electron');
-
 const form = document.querySelector('#todo-form');
 const input = document.querySelector('#todo-input');
 const dateInput = document.querySelector('#due-date');
@@ -9,107 +7,80 @@ const list = document.querySelector('#todo-list');
 let tasks = [];
 
 function renderTasks() {
-  list.innerHTML = '';
-  tasks.forEach((task, index) => {
-    const li = document.createElement('li');
-    li.className = 'list-group-item d-flex justify-content-between align-items-center';
-    li.innerHTML = `
-      <span>${task.name}</span>
-      <span>${task.dueDate ? task.dueDate + ' ' + task.dueTime : ''}</span>
-      <button class="btn btn-danger delete-task" data-index="${index}">Delete</button>
-    `;
-    list.appendChild(li);
-  });
+    list.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+            <span>${task.name}</span>
+            <span>${task.dueDate ? task.dueDate + ' ' + task.dueTime : ''}</span>
+            <button class="btn btn-danger delete-task" data-index="${index}">Delete</button>
+        `;
+        list.appendChild(li);
+
+        // Delete task event listener
+        const deleteBtn = li.querySelector('.delete-task');
+        deleteBtn.addEventListener('click', () => {
+            tasks.splice(index, 1);
+            saveTasks();
+            renderTasks();
+            ipcRenderer.send('tasks', tasks);
+        });
+    });
 }
 
 function addTask() {
-    var taskInput = document.getElementById("todo-input");
-    var dueDateInput = document.getElementById("due-date");
-    var dueTimeInput = document.getElementById("due-time");
-  
-    var task = taskInput.value;
-    var dueDate = dueDateInput.value;
-    var dueTime = dueTimeInput.value;
-  
-    var todoList = document.getElementById("todo-list");
-  
-    var li = document.createElement("li");
-    li.className = "list-group-item";
-  
-    var taskSpan = document.createElement("span");
-    taskSpan.className = "task";
-    taskSpan.innerHTML = task;
-    li.appendChild(taskSpan);
-  
-    var dueDateSpan = document.createElement("span");
-    dueDateSpan.className = "due-date";
-    dueDateSpan.innerHTML = "Due Date: " + dueDate;
-    li.appendChild(dueDateSpan);
-  
-    var dueTimeSpan = document.createElement("span");
-    dueTimeSpan.className = "due-time";
-    dueTimeSpan.innerHTML = "Due Time: " + dueTime;
-    li.appendChild(dueTimeSpan);
-  
-    todoList.appendChild(li);
-  
-    taskInput.value = "";
-    dueDateInput.value = "";
-    dueTimeInput.value = "";
+    try {
+      const taskName = input.value.trim();
+      const dueDate = dateInput.value.trim();
+      const dueTime = timeInput.value.trim();
+      
+      // Check if the due date is in the future
+      const today = new Date();
+      const selectedDate = new Date(dueDate + ' ' + dueTime);
+      if (selectedDate < today) {
+        throw new Error('The due date cannot be in the past.');
+      }
+      
+      if (taskName) {
+        tasks.push({
+          name: taskName,
+          dueDate,
+          dueTime
+        });
+        saveTasks();
+        renderTasks();
+      } else {
+        alert('Please enter a task name.');
+      }
+    } catch (err) {
+        alert(err.message);
+        input.value = '';
+        dateInput.value = '';
+        timeInput.value = '';
+      }
   }
 
 function saveTasks() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-
-function addTaskToPage(task) {
-  const li = document.createElement('li');
-  li.className = 'list-group-item d-flex justify-content-between align-items-center';
-  li.innerHTML = `
-    <span>${task.name}</span>
-    <span>${task.dueDate ? task.dueDate + ' ' + task.dueTime : ''}</span>
-    <button class="btn btn-danger delete-task" data-id="${task.id}">Delete</button>
-  `;
-  list.appendChild(li);
-}
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const taskName = input.value.trim();
-  const dueDate = dateInput.value.trim();
-  const dueTime = timeInput.value.trim();
-  if (taskName) {
-    addTask(taskName, dueDate, dueTime);
-    input.value = '';
-    dateInput.value = '';
-    timeInput.value = '';
-    ipcRenderer.send('tasks', tasks);
-  }
-});
-
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete-task')) {
-    const taskId = parseInt(e.target.getAttribute('data-id'));
-    tasks = tasks.filter(task => task.id !== taskId);
-    saveTasks();
-    renderTasks();
-    ipcRenderer.send('tasks', tasks);
-  }
-});
-
-ipcRenderer.on('tasks', (event, updatedTasks) => {
-  tasks = updatedTasks;
-  renderTasks();
-});
 
 // Initialize the tasks array from local storage on page load
 const storedTasks = localStorage.getItem('tasks');
 if (storedTasks) {
-  tasks = JSON.parse(storedTasks);
-  renderTasks();
+    tasks = JSON.parse(storedTasks);
+    renderTasks();
 }
 
-document.getElementById("todo-form").addEventListener("submit", function(event) {
-    event.preventDefault();
+// Set the value of the date input to the current date
+const today = new Date();
+const todayString = today.toISOString().substr(0, 10);
+dateInput.value = todayString;
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
     addTask();
-  });
+    input.value = '';
+    dateInput.value = '';
+    timeInput.value = '';
+});
